696ba69e8390b0382fc535cff45357cc6496fb1c
lijingang <hh2019@qq.com>
2023-08-15 11:33:41 +0800

    up

--- glados_Qinglong.py
+++ glados_Qinglong.py
@@ -1,10 +1,10 @@
 import requests,json,os
 
 # pushplus秘钥
-sckey = os.environ。get("PUSHPLUS_TOKEN"， "")
+sckey = os.environ.get("PUSHPLUS_TOKEN", "")
 sendContent = ''
 # glados账号cookie
-cookies= os.environ。get("GLADOS_COOKIE"， [])。split("&")
+cookies= os.environ.get("GLADOS_COOKIE", []).split("&")
 if cookies[0] == "":
     print('未获取到COOKIE变量') 
     cookies = []
@@ -20,14 +20,14 @@ def start():
     payload={
         'token': 'glados.one'
     }
-    for cookie 在 cookies:
+    for cookie in cookies:
         checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
         state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
     #--------------------------------------------------------------------------------------------------------#  
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         email = state.json()['data']['email']
-        if 'message' 在 checkin.text:
+        if 'message' in checkin.text:
             mess = checkin.json()['message']
             print(email+'----'+mess+'----剩余('+time+')天')  # 日志输出
             global sendContent
